810ac9b600ddb526ed921e2e4c06ca881c3ea83c
ehreang-svg <ehreang@gmail.com>
2026-06-27 23:35:55 +0700

    config.js

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ window.TABUNGAN_API = "https://script.google.com/macros/s/AKfycbyCWL9Xrt-Dq4Cc4e
 window.JADWAL_API = "https://script.google.com/macros/s/AKfycbzE8W8Rd6coOfao7YLVZgW9-CWVPe6Nl3TQ0qy6X0r04oLbK1_brfl8yCq7HYysOSILMA/exec";
 window.Quiz_API ="https://script.google.com/macros/s/AKfycbx76vMYaeEvwBdtM0sDrqFPDN7n78Z9qMVysyloix652KBctCHysJYRYyz3g2XjZWZ7/exec";
 
-const HARI_LIBUR = ["2026-01-01","2026-01-27","2026-02-19","2026-03-29","2026-04-18","2026-05-01","2026-05-12","2026-06-01","2026-08-17","2026-12-25"];
 
 let history = [];
 let currentUser = null;
