38b96974212093123e4006cd60d6dd605c6959dc
ehreang-svg <ehreang@gmail.com>
2026-06-29 22:36:57 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+window.API_URL = "https://script.google.com/macros/s/AKfycbwFHgAK8rezu-3WjL_FWS6EN1GXDrDqRS81iu0KTArcGx8zwoVXJCT_zK2SIGEM0qGq/exec";
+window.ABSEN_API = "https://script.google.com/macros/s/AKfycbxIdtImYj2YCJEcW2Q2dfn9pkjzgq88qnhB_kdln021rUdmqJEh2jQiz1kL0qeYRI8l/exec";
+window.TABUNGAN_API = "https://script.google.com/macros/s/AKfycbyCWL9Xrt-Dq4Cc4eah9OSYqYCGxY9tChXYYPSwOKwysNkZo4pgDbBVe6-YYDPmjVY/exec";
+window.JADWAL_API = "https://script.google.com/macros/s/AKfycbzGPnqxz0Fx9jQnPAm6OguEdN2yQ6ZvccVC6lb1KEovso4gVZDywLNZhTOsEHN3_VwL/exec";
+window.Quiz_API ="https://script.google.com/macros/s/AKfycbx76vMYaeEvwBdtM0sDrqFPDN7n78Z9qMVysyloix652KBctCHysJYRYyz3g2XjZWZ7/exec";
+
+const HARI_LIBUR = ["2026-01-01","2026-01-27","2026-02-19","2026-03-29","2026-04-18","2026-05-01","2026-05-12","2026-06-01","2026-08-17","2026-12-25"];
+
+let history = [];
+let currentUser = null;
+let dataSiswaGlobal = [];
+let dataSiswaTabungan = [];
+let dataSiswaRaport = [];
+let dataSiswaKognitif = [];
+let dataSoal=[];
+let dataIjazah = [];
+let dataIjazahMD = [];
