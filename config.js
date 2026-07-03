aeac8a6841f30a529f5b9514fe59df76e71eeb5c
ehreang-svg <ehreang@gmail.com>
2026-07-03 13:53:01 +0700

    config.js

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ window.TABUNGAN_API = "https://script.google.com/macros/s/AKfycbyCWL9Xrt-Dq4Cc4e
 window.JADWAL_API = "https://script.google.com/macros/s/AKfycbzGPnqxz0Fx9jQnPAm6OguEdN2yQ6ZvccVC6lb1KEovso4gVZDywLNZhTOsEHN3_VwL/exec";
 window.Quiz_API ="https://script.google.com/macros/s/AKfycbx76vMYaeEvwBdtM0sDrqFPDN7n78Z9qMVysyloix652KBctCHysJYRYyz3g2XjZWZ7/exec";
 window.KBM_API ="https://script.google.com/macros/s/AKfycbwz89jBhPTie4I13_YSgRkM4LTkcVuTZ0cWs1TtYeY37s6pRR0_muTXGpV5es6zboDs/exec";
+window.CHAT_API ="https://script.google.com/macros/s/AKfycby7YTM71QxM1khY_FDxkwh-Fu5F3kOT4EJBWzVM1GrO8ZGphfCCCeydfuL8-xKBzDh5/exec";
 
 const HARI_LIBUR = ["2026-01-01","2026-01-27","2026-02-19","2026-03-29","2026-04-18","2026-05-01","2026-05-12","2026-06-01","2026-08-17","2026-12-25"];
 
