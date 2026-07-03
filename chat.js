23fd8cf9127666a458e5ea62068dc2326170c9e8
ehreang-svg <ehreang@gmail.com>
2026-07-03 13:56:24 +0700

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,102 @@
+async function kirimChat(){
+
+try{
+
+const pesan=document
+.getElementById("chatText")
+.value.trim();
+
+if(!pesan) return;
+
+const nama=localStorage.getItem("nama");
+
+const foto=localStorage.getItem("foto");
+
+await fetch(
+
+CHAT_API+
+"?action=kirimChat"+
+"&nama="+encodeURIComponent(nama)+
+"&foto="+encodeURIComponent(foto)+
+"&pesan="+encodeURIComponent(pesan)
+
+);
+
+document
+.getElementById("chatText")
+.value="";
+
+loadChat();
+
+}catch(err){
+
+alert(err);
+
+}
+
+}
+async function loadChat(){
+
+try{
+
+const res=await fetch(
+
+CHAT_API+
+"?action=getChat"
+
+);
+
+const result=await res.json();
+
+if(!result.status) return;
+
+const box=document
+.getElementById("chatList");
+
+box.innerHTML="";
+
+result.data.forEach(item=>{
+
+box.innerHTML+=`
+
+<div class="chatItem">
+
+<img src="${item.foto}">
+
+<div>
+
+<div class="chatNama">
+
+${item.nama}
+
+</div>
+
+<div class="chatPesan">
+
+${item.pesan}
+
+</div>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}catch(err){
+
+console.log(err);
+
+}
+
+}
+setInterval(loadChat,2000);
+function openChat(){
+
+showPage("chatPage");
+
+loadChat();
+
+}
